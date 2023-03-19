53c2adb26f9ae34cdc2ece056738f116ebc75379
ghislainvava <ghislainvachet@yahoo.fr>
2023-03-19 17:36:57 +0100

    j'y crois

--- migrations/Version20230319163604.php
+++ migrations/Version20230319163604.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230319162524 extends AbstractMigration
+final class Version20230319163604 extends AbstractMigration
 {
     public function getDescription(): string
     {
