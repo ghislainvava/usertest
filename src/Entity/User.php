4d7c262d78673c9818648c984e86c11051c14b81
Nom d'utilisateur générique <votre-email@example.com>
2023-05-10 13:05:48 +0200

    installation vich

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -20,7 +20,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(length: 180, unique: true)]
     private ?string $email = null;
 
-    #[ORM\Column]
+    #[ORM\Column(length: 255)]
     private array $roles = [];
 
     /**
